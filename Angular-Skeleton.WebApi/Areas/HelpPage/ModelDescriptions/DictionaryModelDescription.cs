9d9e603ca0ec01cda6490b7b2ba4d3b1bb3d27d6
Heemi Hanif <iheemi@hotmail.com>
2016-05-03 16:54:39 +0800

    addedd web api

---
+++ Angular-Skeleton.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Angular_Skeleton.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Angular-Skeleton.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Angular_Skeleton.WebApi.WebApiApplication" Language="C#" %>
